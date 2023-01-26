65dba8a8bc8318fe3f5a90773f3b136474f2d013
Luc Mosser <luc.mosser86@orange.fr>
2023-01-26 16:51:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+
+  
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [bitcoinPrice, setBitcoinPrice] = useState(null);
+
+  const fetchPrice = () => {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        setBitcoinPrice(response.data.bpi.USD.rate);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      <div className="card">
+        {bitcoinPrice ? (
+          <p>Aujourd'hui le BTC vaut ${bitcoinPrice}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+        <button className="button button-img" onClick={fetchPrice}>
+          Hop!
+        </button>
+      </div>
     </div>
   );
 }
